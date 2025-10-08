cc82f16c4ebd06da0a2cff559e350bf90de527b6
brendayw <brendayw97@gmail.com>
2025-10-07 21:11:43 -0300

    Guardando cambios antes del pull

--- src/test/java/ar/edu/utn/frbb/tup/ApplicationTests.java
+++ src/test/java/ar/edu/utn/frbb/tup/ApplicationTests.java
@@ -2,11 +2,14 @@ package ar.edu.utn.frbb.tup;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class ApplicationTests {
 
     @Test
     void contextLoads() {
     }
+
 }
